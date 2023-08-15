44e29192e2dc9db22073a84369971d0f64d4d83f
jakob <jakobnonnenmacher@hotmail.com>
2023-08-15 02:16:10 +0200

    service die Links zu klicken funktioniert und Datenbankanbindung steht

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,37 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SearchDriveClassComponent } from './search-drive-class/search-drive-class.component';
+import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import { DrivingSchoolComponent } from './search-drive-class/driving-school/driving-school.component';
+import { SchoolDetailViewComponent } from './search-drive-class/driving-school/school-detail-view/school-detail-view.component';
+import { TrimPipe } from './trim.pipe';
+import { RouterModule, Routes } from '@angular/router';
+import { SchoolService } from './school.service';
+import { HttpClientModule } from '@angular/common/http';
+
+const appRoutes: Routes = [
+  { path: '', component: SearchDriveClassComponent },
+  { path: 'school/:id', component: SchoolDetailViewComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchDriveClassComponent,
+    HeaderComponent,
+    DrivingSchoolComponent,
+    SchoolDetailViewComponent,
+    TrimPipe,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
