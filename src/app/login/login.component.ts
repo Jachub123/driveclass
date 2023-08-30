a58a9d8133c4be89107dbfa7ef524ad4feeb9d42
jakob <jakobnonnenmacher@hotmail.com>
2023-08-30 06:20:05 +0200

    added files

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../register/auth-service.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent implements OnInit {
+  constructor(private authService: AuthService) {}
+  errorMsg: string;
+  logout() {
+    this.authService.logout();
+  }
+  login(form) {
+    this.authService.login(form.value.email, form.value.password);
+  }
+
+  ngOnInit(): void {
+    this.authService.eventAuthError.subscribe((error) => {
+      this.errorMsg = error;
+    });
+  }
+}
