b528a7e4b61aa417be7b700ff92955176cef422a
jakob <jakobnonnenmacher@hotmail.com>
2023-09-03 22:54:21 +0200

    facebook und google anmeldung implementiert

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../register/auth-service.service';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { FacebookAuthProvider } from '@angular/fire/auth';
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  browserPopupRedirectResolver,
+} from 'firebase/auth';
+import { environment } from 'src/environments/environment';
+import { FirebaseApp } from '@angular/fire/app';
 
 @Component({
   selector: 'app-login',
@@ -7,13 +17,32 @@ import { AuthService } from '../register/auth-service.service';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private auth3: AngularFireAuth,
+    private firebase: FirebaseApp
+  ) {}
+
   errorMsg: string;
   logout() {
     this.authService.logout();
   }
   login(form) {
     this.authService.login(form.value.email, form.value.password);
+  }
+
+  async facebookLogin() {
+    const result = await this.auth3.signInWithPopup(new FacebookAuthProvider());
+    console.log(result);
+  }
+  async googleLogin() {
+    const auth = getAuth(this.firebase);
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider, browserPopupRedirectResolver).then(
+      (result) => {
+        console.log(result.user);
+      }
+    );
   }
 
   ngOnInit(): void {
